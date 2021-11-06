bdcdae2ba1dc651f9fe344200b8ffd2bb18542c9
Dimitri Lopez <dimitrilopez01@gmail.com>
2021-11-06 18:06:52 -0400

    Adding more pip stuff

--- app.py
+++ app.py
@@ -15,7 +15,6 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
-
 app.layout = html.Div(children = [
      html.H1(children='Spotify Data'),
      # html.Div(children='''Enter an Artist Name:'''),
