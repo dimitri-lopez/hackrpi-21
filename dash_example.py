664b892c84bacc7982f09c237c1cc62452c65495
Dimitri Lopez <dimitrilopez01@gmail.com>
2021-11-06 16:00:19 -0400

    Testing Horuku deployment

--- dash_example.py
+++ dash_example.py
@@ -9,7 +9,11 @@ from dash import dcc
 import plotly.express as px
 import pandas as pd
 
-app = dash.Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
@@ -18,9 +22,7 @@ df = pd.DataFrame({
     "Amount": [4, 1, 2, 2, 4, 5],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
-
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
@@ -33,6 +35,7 @@ app.layout = html.Div(children=[
         figure=fig
     )
 ])
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
